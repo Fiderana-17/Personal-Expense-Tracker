7970aaa9eba836a8518609ff550ac365b715afed
Mena845 <tsikiniainatsilavina@gmail.com>
2025-08-26 14:44:52 +0300

    Add api to get expense by id

--- backend/src/controllers/expense.controller.js
+++ backend/src/controllers/expense.controller.js
@@ -16,4 +16,22 @@ export const getExpensesByUser = async (req, res) => {
   }
 };
 
+//  Récupérer une dépense par ID
+export const getExpenseById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const expense = await prisma.expense.findUnique({
+      where: { id: parseInt(id) },
+      include: { category: true, receipt: true },
+    });
+
+    if (!expense) return res.status(404).json({ message: 'Expense not found' });
+
+    res.status(200).json(expense);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching expense', error });
+  }
+};
+
 
