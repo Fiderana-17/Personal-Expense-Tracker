ff1fcebb001d049878efff55b24373cea67a0d88
Mena845 <tsikiniainatsilavina@gmail.com>
2025-08-27 10:26:31 +0300

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
+
 import express from "express";
 import cors from "cors";
+import dotenv from 'dotenv';
 import authRoutes from './src/routes/auth.routes.js';
 import categoryRoutes from "./src/routes/category.routes.js";
+import expenseRoutes from "./src/routes/expense.routes.js";
 
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +17,7 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', authRoutes);
 app.use("/api/categories", categoryRoutes);
+app.use('/api/expenses', expenseRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
