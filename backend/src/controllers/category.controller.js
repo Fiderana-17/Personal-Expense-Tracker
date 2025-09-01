5e9b90caaeaa2bf49b1746609d2d64c9feb34b08
Fiderana-17 <hei.fiderana@gmail.com>
2025-09-01 09:52:54 +0300

    feat: implement category creation with validation

--- backend/src/controllers/category.controller.js
+++ backend/src/controllers/category.controller.js
@@ -11,3 +11,26 @@ export const getCategories = async (req, res) => {
   }
 };
 
+
+
+// Crée une nouvelle catégorie
+export const createCategory = async (req, res) => {
+  try {
+    const { name, userId } = req.body;
+    if (!name || !userId) {
+      return res.status(400).json({ message: "Le champ 'name' et 'userId' sont requis" });
+    }
+
+    const category = await prisma.category.create({
+      data: {
+        name,
+        userId: parseInt(userId) // Assure-toi que c’est bien un entier
+      }
+    });
+
+    res.status(201).json({ message: "Category created", data: category });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Erreur lors de la création de la catégorie", error });
+    }
+};
