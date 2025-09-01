222f4bd8746dba32bda5fff58baade940dd31334
Fiderana-17 <hei.fiderana@gmail.com>
2025-09-01 14:16:28 +0300

    feat(category): création du composant CategoryPage avec affichage des catégories

---
+++ frontend/src/components/Categories/CategoriesList.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useMemo, useState } from "react";
+import { Plus, Search, Edit, Trash2, FolderOpen, Tag } from "lucide-react";
+import {
+  getAllCategories,
+  createCategory,
+  deleteCategory,
+  updateCategory,
+  type Category,
+} from "../../api/category";
+import CategoryForm from "./CategoryForm";
+
+const CategoryPage: React.FC = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  // gestion du formulaire
+  const [showForm, setShowForm] = useState(false);
+  const [mode, setMode] = useState<"create" | "edit">("create");
+  const [editing, setEditing] = useState<Pick<Category, "id" | "name" | "userId"> | null>(null);
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const fetchCategories = async () => {
+    try {
+      const data = await getAllCategories();
+      setCategories(data);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
