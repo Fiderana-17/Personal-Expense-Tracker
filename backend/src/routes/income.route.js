897a2411befb595a46ae088464421c9f4c7ed597
Mitia Jessica <mitiah.jessica@gmail.com>
2025-09-04 16:41:58 +0300

    add authentificationToken in the income.route

--- backend/src/routes/income.route.js
+++ backend/src/routes/income.route.js
@@ -2,6 +2,9 @@ import express from "express"
 const router = express.Router();
 
 import { getAllIncomes, getIncomeById, createIncome, updateIncome, deleteIncome } from "../controllers/income.controller.js";
+import { authenticateToken } from "../middleware/auth.js";
+
+router.use(authenticateToken);
 
 router.get("/", getAllIncomes);
 router.get("/:id", getIncomeById);
