96c18a8c0be8190c404e7af6770ee01bfe6840af
Irina <irinarakoto2005@gmail.com>
2025-09-05 21:21:25 +0300

    fix: change res.json() to responseData from category causing bug

--- frontend/src/api/category.ts
+++ frontend/src/api/category.ts
@@ -64,7 +64,7 @@ export async function createCategory(data: Partial<Category>): Promise<{ message
       ...responseData
     };
   }
-  return res.json();
+  return responseData;;
 }
 
 // UPDATE category
