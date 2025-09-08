b6a3a7d424c0861938978e704d106566ddd01817
Irina <irinarakoto2005@gmail.com>
2025-09-08 21:54:56 +0300

    refactor: relocate i18n.ts into utils directory

--- frontend/src/utils/i18n.ts
+++ frontend/src/utils/i18n.ts
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import en from "./translations/en/global.json";
-import fr from "./translations/fr/global.json";
+import en from "@/translations/en/global.json";
+import fr from "@/translations/fr/global.json";
 
 i18n.use(initReactI18next).init({
   resources: {
