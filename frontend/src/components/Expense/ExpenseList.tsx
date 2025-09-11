a9cf7ff05ed93053878dbd9cdd2ed299eca3d2fd
Mitia Jessica <mitiah.jessica@gmail.com>
2025-09-11 15:09:13 +0300

    change style of the total summary

--- frontend/src/components/Expense/ExpenseList.tsx
+++ frontend/src/components/Expense/ExpenseList.tsx
@@ -529,7 +529,7 @@ const ExpensesList: React.FC = () => {
         )}
       </AnimatePresence>
 
-      {filteredExpenses.length > 0 && (
+      {filteredExpenses.length >= 0 && (
         <div className="bg-gradient-to-t from-red-500 to-red-700 rounded-xl shadow-md p-6 text-white">
           <div className="flex justify-between items-center">
             <div>
