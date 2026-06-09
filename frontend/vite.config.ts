1cddaedcdd1394410c585d9ae7547c633b4d6f99
Fiderana-17 <hei.fiderana@gmail.com>
2026-06-09 10:16:16 +0300

    fix: fix error of CORS

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,14 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    alias: { "@": path.resolve(__dirname, "./src") },
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
     },
   },
 })
